3d367a19dddd67667bb448bf6bb4593c0e7d3f65
Chystiakov73Viktor <130795120+Chystiakov73Viktor@users.noreply.github.com>
2023-12-09 20:46:52 +0200

    Classes in TypeScript

--- src/2.ts
+++ src/2.ts
@@ -10,17 +10,13 @@
 
   Клас Manager повинен бути підклас класу Employee
 
-  Необхідно реалізувати в класі Manager конструктор, який викликатиме конструктор суперкласу та збільшуватиме salary на 10000.
+катиме конструктор суперкласу та збільшуватиме salary на 10000.  Необхідно реалізувати в класі Manager конструктор, який викли
 
 */
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
-
-  constructor(name: string, department: string, salary: number) {
+  constructor( public name: string, private department: string, protected salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,6 +29,9 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
+  }
 }
 
 
